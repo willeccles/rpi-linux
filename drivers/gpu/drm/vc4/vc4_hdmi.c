a72e27abc4d45a5f74de92ae0103ef63f20fe600
Maxime Ripard <maxime@cerno.tech>
2021-08-19 15:53:03 +0200

    drm/vc4: select PM
    We already depend on runtime PM to get the power domains and clocks for
    most of the devices supported by the vc4 driver, so let's just select it
    to make sure it's there, and remove the ifdef.
    
    Signed-off-by: Maxime Ripard <maxime@cerno.tech>

--- drivers/gpu/drm/vc4/vc4_hdmi.c
+++ drivers/gpu/drm/vc4/vc4_hdmi.c
@@ -2112,7 +2112,6 @@ static int vc5_hdmi_init_resources(struct vc4_hdmi *vc4_hdmi)
 	return 0;
 }
 
-#ifdef CONFIG_PM
 static int vc4_hdmi_runtime_suspend(struct device *dev)
 {
 	struct vc4_hdmi *vc4_hdmi = dev_get_drvdata(dev);
@@ -2133,7 +2132,6 @@ static int vc4_hdmi_runtime_resume(struct device *dev)
 
 	return 0;
 }
-#endif
 
 static int vc4_hdmi_bind(struct device *dev, struct device *master, void *data)
 {
